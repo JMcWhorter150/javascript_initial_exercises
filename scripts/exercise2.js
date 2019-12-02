f3c1d0532cdba1b7d319220485536d40307f8903
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-02 15:28:15 -0500

    Adds printBox function exercise

--- scripts/exercise2.js
+++ scripts/exercise2.js
@@ -7,13 +7,46 @@ function printNumbers(startNumber, endNumber) {
 // printNumbers(1, 10);
 
 function printSquare(size) {
+    // prints the the *** string size times
     for (let i = 0; i<size; i++) {
-        let emptyString = ""
+        let emptyString = "";
+        // creates the * string to print
         for (let j = 0; j<size; j++) {
-            emptyString += "*"
+            emptyString += "*";
         }
         console.log(emptyString);
     }
 }
 
 // printSquare(6);
+
+function printBox(width, height) {
+    let emptyString = "";
+    // creates top and bottom parts of box made of *
+    for (let i = 0; i<width; i++) {
+        emptyString += "*";
+    }
+    // prints top * of the box
+    console.log(emptyString);
+    // prints middle lines of box
+    for (let j = 0; j<(height - 2); j++) {
+        // starts the * with an initial star
+        let middleString = "*";
+        // adds the correct amount of empty space
+        for (let h = 0; h<width - 2; h++) {
+            middleString += " ";
+        }
+        // adds the last * on the far side
+        middleString += "*";
+        // prints the actual middle line each time
+        console.log(middleString);
+    }
+    // prints the bottom * of the box
+    console.log(emptyString);
+}
+
+printBox(6, 4);
+
+function printBanner(text) {
+    for () {}
+}
