bf4974a7a32c5620d1474cba0315a1b2e1b9e33f
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-02 16:13:31 -0500

    Adds cipher and leetspeak functions

--- scripts/exercise2.js
+++ scripts/exercise2.js
@@ -72,3 +72,48 @@ function factors(number) {
 
 // to find the factors of 120, I first start at 1, then I divide 120 by 1. If it is divisible by 1, I add 1 to the list. Then I go to 2. If 120 divided by number has no remainder, then I ........
 // console.log(factors(120));
+
+function cipher(string, offset) {
+    let cleanString = string.toLowerCase();
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let newString = "";
+    for (let character of cleanString) {
+        if (character === " ") {
+            newString += " ";
+        } else {
+            newString += alphabet[(alphabet.indexOf(character) + offset) % 26];
+        }
+    }
+    return newString;
+}
+
+// console.log(cipher('Genius without education is like silver in the mine', 13))
+
+// console.log(cipher('travhf jvgubhg rqhpngvba vf yvxr fvyire va gur zvar', 13))
+
+function leetspeak(string) {
+    string = string.toUpperCase();
+    let leetString = "";
+    for (let character of string) {
+        if (character === "A") {
+            leetString += "4";
+        } else if (character === "E") {
+            leetString += "3";
+        } else if (character === "G") {
+            leetString += "6";
+        } else if (character === "I") {
+            leetString += "1";
+        } else if (character === "O") {
+            leetString += "0";
+        } else if (character === "S") {
+            leetString += "5";
+        } else if (character === "T") {
+            leetString += "7";
+        } else {
+            leetString += character;
+        }
+    }
+    return leetString
+}
+
+// console.log(leetspeak("this is a sentence of random letters"));
