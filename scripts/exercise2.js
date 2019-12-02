143ec7020fc09e28aac0c38de8fd1ceaddc45d2b
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-02 15:13:26 -0500

    Adds completed print square exercise

--- scripts/exercise2.js
+++ scripts/exercise2.js
@@ -4,4 +4,16 @@ function printNumbers(startNumber, endNumber) {
     }
 }
 
-printNumbers(1, 10);
+// printNumbers(1, 10);
+
+function printSquare(size) {
+    for (let i = 0; i<size; i++) {
+        let emptyString = ""
+        for (let j = 0; j<size; j++) {
+            emptyString += "*"
+        }
+        console.log(emptyString);
+    }
+}
+
+// printSquare(6);
